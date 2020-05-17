ae73ecda82fa1eb6681660dbb0208e15c5af82c6
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-17 11:27:40 +0530

    Java Based - Initializtion of spring container as standalone

---
+++ JavaBasedConfigurationMetadata/src/main/java/spring/AccountMain.java
@@ -0,0 +1,37 @@
+package spring;
+
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.configuration.AccountConfiguration;
+import spring.model.Account;
+import spring.service.AccountService;
+
+import java.time.LocalDateTime;
+
+public class AccountMain {
+    @Autowired
+    static BeanFactory applicationContext;
+
+    public static void main(String[] args) {
+        /***Java Based Configuration metadata ***/
+       // ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AccountConfiguration.class);
+        AccountService accountService = applicationContext.getBean("accountService",AccountService.class);
+
+        Account account = new Account(1, "Ganga", 60000, LocalDateTime.now());
+        Account account2 = new Account(2, "Ashwin", 30000, LocalDateTime.now());
+
+        System.out.println("Java based configuration Metadata ");
+        accountService.addAccount(account);
+        accountService.addAccount(account2);
+
+        System.out.println("Before transfer : ");
+        System.out.println(account.getOwnerName() + " : " + account.getBalance() + "\n" + account2.getOwnerName() + " : " + account2.getBalance());
+        System.out.println("After transferring 10000");
+        accountService.transferMoney(1, 2, 10000);
+        System.out.println(account.getOwnerName() + " : " + account.getBalance() + "\n" + account2.getOwnerName() + " : " + account2.getBalance());
+    }
+}
