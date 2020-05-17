311adaaeac1eeb9745dc1f3c8dd9f5c0e8f673e7
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-17 11:19:21 +0530

    XML Based - Initializing spring container as standalone

---
+++ XMLBasedConfigurationMetadata/src/main/java/spring/AccountMain.java
@@ -0,0 +1,31 @@
+package spring;
+
+import spring.model.Account;
+import spring.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.time.LocalDateTime;
+
+public class AccountMain {
+    public static void main(String[] args) {
+
+
+        /***XML based Configuration Metadata***/
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        AccountService accountService = context.getBean("accountService", AccountService.class);
+
+        Account account = new Account(1, "Ganga", 60000, LocalDateTime.now());
+        Account account2 = new Account(2, "Ashwin", 30000, LocalDateTime.now());
+        accountService.addAccount(account);
+        accountService.addAccount(account2);
+
+        System.out.println("Before transfer : ");
+        System.out.println(account.getOwnerName() + " : " + account.getBalance() + "\n" + account2.getOwnerName() + " : " + account2.getBalance());
+        System.out.println("After transfering 10000");
+        accountService.transferMoney(1, 2, 10000);
+        System.out.println(account.getOwnerName() + " : " + account.getBalance() + "\n" + account2.getOwnerName() + " : " + account2.getBalance());
+
+    }
+}
