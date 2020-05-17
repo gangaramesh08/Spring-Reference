9c73d78bae9dc394cc45c36f6c9c981b5e5e173e
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-17 11:27:01 +0530

    Java Based - bean configuration class added

---
+++ JavaBasedConfigurationMetadata/src/main/java/spring/configuration/AccountConfiguration.java
@@ -0,0 +1,27 @@
+package spring.configuration;
+
+import spring.dao.AccountDao;
+import spring.dao.AccountDaoInMemoryImpl;
+import spring.service.AccountService;
+import spring.service.AccountServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+public class AccountConfiguration {
+
+    @Bean
+    @DependsOn("accountDao")
+    public AccountService accountService(){
+        AccountServiceImpl accountServicebean = new AccountServiceImpl();
+        accountServicebean.setAccountDao(accountDao());
+        return accountServicebean;
+    }
+
+    @Bean
+    public AccountDao accountDao(){
+        AccountDaoInMemoryImpl accountDaoInMemorybean = new AccountDaoInMemoryImpl();
+        return accountDaoInMemorybean;
+    }
+}
