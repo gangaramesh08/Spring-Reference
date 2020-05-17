fc4c8aff75fb9bc8edd6d0130a4ce95a6c9541ed
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-17 11:21:44 +0530

    Annotation Based - InMemoryDao Layer added

---
+++ AnnotationBased/src/main/java/spring/dao/AccountDao.java
@@ -0,0 +1,26 @@
+package spring.dao;
+
+
+
+import org.springframework.stereotype.Repository;
+import spring.model.Account;
+
+import java.util.List;
+
+@Repository
+public interface AccountDao {
+
+    void insert(Account account);
+
+    void update(Account account);
+
+    void update(List<Account> accounts);
+
+    void delete(long accountId);
+
+    Account find(long accountId);
+
+    List<Account> find(List<Long> accountIds);
+
+    List<Account> find(String ownerName);
+}
