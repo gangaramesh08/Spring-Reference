b1af15ba719350c3256a574ca8f6630d2b150b29
gangaramesh08 <gangaramesh08@gmail.com>
2020-05-17 11:14:40 +0530

    XML Based - POJO created

---
+++ XMLBasedConfigurationMetadata/src/main/java/spring/model/Account.java
@@ -0,0 +1,49 @@
+package spring.model;
+
+import java.time.LocalDateTime;
+
+public class Account {
+    private long id;
+    private String ownerName;
+    private double balance;
+    private LocalDateTime accessTime;
+
+    public Account(long id, String ownerName, double balance, LocalDateTime accessTime) {
+        this.id = id;
+        this.ownerName = ownerName;
+        this.balance = balance;
+        this.accessTime = accessTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getAccessTime() {
+        return accessTime;
+    }
+
+    public void setAccessTime(LocalDateTime accessTime) {
+        this.accessTime = accessTime;
+    }
+}
